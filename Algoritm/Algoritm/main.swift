c546477ccdcdbcb79007bb9e2901a47d4a6fe4bb
commit <>
2018-01-18 16:39:19 +0900

    add 3

--- Algoritm/Algoritm/main.swift
+++ Algoritm/Algoritm/main.swift
@@ -8,98 +8,36 @@
 
 import Foundation
 
-
-
 /*
- 6. 리펙토링하기 처음 이프문
- func strStr(_ haystack: String, _ needle: String) -> Int {
- if haystack.count == 0 && needle.count == 0 {
- return 0
- } else if haystack.count != 0 && needle.count == 0 {
- return 0
- }
- 
- if haystack.range(of: needle) != nil {
- let range = haystack.range(of: needle)
- let location = haystack.distance(from: haystack.startIndex, to: (range?.lowerBound)!)
- return location
- } else {
- return -1
- }
- }
- 
- print(strStr("hello", "llo"))
- */
-
+ 11.
+func formatMoney(_ val:Double) -> String {
+    
+    var str = "$"
+    
+    str = str + String(format: "%.2f",val)
+    return str
+}
+*/
 /*
- 7.
- func searchInsert(_ nums: [Int], _ target: Int) -> Int {
- var newNums = nums
- 
- if let index = newNums.index(of: target) {
- return index
- } else {
- newNums.append(target)
- newNums.sort()
- return searchInsert(newNums, target)
- }
- }
- 
- print(searchInsert([1,3,5,6], 5))
- print(searchInsert([1,3,5,6], 2))
- print(searchInsert([1,3,5,6], 7))
- print(searchInsert([1,3,5,6], 0))
- 
- */
-
-
-/*
- 8.
-
-func sumOfIntegersInString(_ string: String) -> Int {
-    var arr = string.components(separatedBy: CharacterSet.decimalDigits.inverted)
-    var result = 0
-    for i in 0..<arr.count {
-        if let num = Int(arr[i]) {
-            result += num
+ 12.
+func countArare(_ n: Int) -> String {
+    let count = n / 2
+    var str = ""
+    for i in 0..<count {
+        if n == 2 || i == count - 1{
+            str += "adak"
+        } else {
+            str += "adak "
         }
     }
     
-    return result
-}
-
-let string = "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog"
-print(sumOfIntegersInString(string))
-
-*/
-
-/*
- 9.
-func sumOfTwoSmallestIntegersIn(_ array: [Int]) -> Int {
-    var arr = array
-    var value = 0
-    for _ in 0...1{
-        value += arr.min()!
-        let index = arr.index(of: arr.min()!)
-        arr.remove(at: index!)
-    }
     
-    return value
+    
+    if n % 2 != 0 && n == 1 {
+        str += "anane"
+    } else if n % 2 != 0 {
+        str += " anane"
+    }
+    return str
 }
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
